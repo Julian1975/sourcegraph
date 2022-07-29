5659dee76eb173e5a2f207ca2d544bba47580503
Erik Seliger <erikseliger@me.com>
2022-07-29 13:55:27 +0200

    Fixup tests

--- internal/gitserver/client_test.go
+++ internal/gitserver/client_test.go
@@ -38,6 +38,13 @@ import (
 	"github.com/sourcegraph/sourcegraph/lib/errors"
 )
 
+func newMockDB() database.DB {
+	db := database.NewMockDB()
+	gr := database.NewMockGitserverRepoStore()
+	db.GitserverReposFunc.SetDefaultReturn(gr)
+	return db
+}
+
 func TestClient_RequestRepoMigrate(t *testing.T) {
 	repo := api.RepoName("github.com/sourcegraph/sourcegraph")
 	addrs := []string{"172.16.8.1:8080", "172.16.8.2:8080"}
@@ -67,7 +74,7 @@ func TestClient_RequestRepoMigrate(t *testing.T) {
 				return nil, errors.Newf("unexpected URL: %q", r.URL.String())
 			}
 		}),
-		database.NewMockDB(),
+		newMockDB(),
 		addrs,
 	)
 
@@ -98,7 +105,7 @@ func TestClient_Remove(t *testing.T) {
 				return nil, errors.Newf("unexpected URL: %q", r.URL.String())
 			}
 		}),
-		database.NewMockDB(),
+		newMockDB(),
 		addrs,
 	)
 
@@ -141,6 +148,7 @@ func TestClient_Archive(t *testing.T) {
 	srv := httptest.NewServer((&server.Server{
 		Logger:   logtest.Scoped(t),
 		ReposDir: filepath.Join(root, "repos"),
+		DB:       newMockDB(),
 		GetRemoteURLFunc: func(_ context.Context, name api.RepoName) (string, error) {
 			testData := tests[name]
 			if testData.remote != "" {
@@ -156,7 +164,7 @@ func TestClient_Archive(t *testing.T) {
 
 	u, _ := url.Parse(srv.URL)
 	addrs := []string{u.Host}
-	cli := gitserver.NewTestClient(&http.Client{}, database.NewMockDB(), addrs)
+	cli := gitserver.NewTestClient(&http.Client{}, newMockDB(), addrs)
 
 	ctx := context.Background()
 	for name, test := range tests {
@@ -339,7 +347,7 @@ func TestAddrForRepo(t *testing.T) {
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			got, err := gitserver.AddrForRepo(context.Background(), "gitserver", database.NewMockDB(), tc.repo, gitserver.GitServerAddresses{
+			got, err := gitserver.AddrForRepo(context.Background(), "gitserver", newMockDB(), tc.repo, gitserver.GitServerAddresses{
 				Addresses:     addrs,
 				PinnedServers: pinned,
 			})
@@ -446,7 +454,7 @@ func TestClient_P4Exec(t *testing.T) {
 
 			u, _ := url.Parse(server.URL)
 			addrs := []string{u.Host}
-			cli := gitserver.NewTestClient(&http.Client{}, database.NewMockDB(), addrs)
+			cli := gitserver.NewTestClient(&http.Client{}, newMockDB(), addrs)
 
 			rc, _, err := cli.P4Exec(ctx, test.host, test.user, test.password, test.args...)
 			if diff := cmp.Diff(test.wantErr, fmt.Sprintf("%v", err)); diff != "" {
@@ -505,6 +513,7 @@ func TestClient_ResolveRevisions(t *testing.T) {
 		err:   &gitdomain.RevisionNotFoundError{Repo: api.RepoName(remote), Spec: "test-fake-ref"},
 	}}
 
+	db := newMockDB()
 	srv := httptest.NewServer((&server.Server{
 		Logger:   logtest.Scoped(t),
 		ReposDir: filepath.Join(root, "repos"),
@@ -514,12 +523,13 @@ func TestClient_ResolveRevisions(t *testing.T) {
 		GetVCSSyncer: func(ctx context.Context, name api.RepoName) (server.VCSSyncer, error) {
 			return &server.GitRepoSyncer{}, nil
 		},
+		DB: db,
 	}).Handler())
 	defer srv.Close()
 
 	u, _ := url.Parse(srv.URL)
 	addrs := []string{u.Host}
-	cli := gitserver.NewTestClient(&http.Client{}, database.NewMockDB(), addrs)
+	cli := gitserver.NewTestClient(&http.Client{}, db, addrs)
 
 	ctx := context.Background()
 	for _, test := range tests {
@@ -541,7 +551,7 @@ func TestClient_ResolveRevisions(t *testing.T) {
 
 func TestClient_AddrForRepo_UsesConfToRead_PinnedRepos(t *testing.T) {
 	ctx := context.Background()
-	client := gitserver.NewTestClient(&http.Client{}, database.NewMockDB(), []string{"gitserver1", "gitserver2"})
+	client := gitserver.NewTestClient(&http.Client{}, newMockDB(), []string{"gitserver1", "gitserver2"})
 	setPinnedRepos(map[string]string{
 		"repo1": "gitserver2",
 	})
@@ -574,7 +584,7 @@ func setPinnedRepos(pinned map[string]string) {
 
 func TestClient_AddrForRepo_Rendezvous(t *testing.T) {
 	ctx := context.Background()
-	client := gitserver.NewTestClient(&http.Client{}, database.NewMockDB(), []string{"gitserver1", "gitserver2"})
+	client := gitserver.NewTestClient(&http.Client{}, newMockDB(), []string{"gitserver1", "gitserver2"})
 
 	tests := []struct {
 		name     string
@@ -641,7 +651,7 @@ func TestClient_BatchLog(t *testing.T) {
 			body := io.NopCloser(strings.NewReader(strings.TrimSpace(string(encoded))))
 			return &http.Response{StatusCode: 200, Body: body}, nil
 		}),
-		database.NewMockDB(),
+		newMockDB(),
 		addrs,
 	)
 
